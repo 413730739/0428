dd114baa001e83e70a7eeb6d0d472c2de1c77613
413730739 <413730739@o365.tku.edu.tw>
2025-04-28 10:02:00 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let capture;
+let mosaicSize = 20; // 馬賽克方塊的大小
 
 function setup() {
   createCanvas(windowWidth, windowHeight); // 使用視窗大小
@@ -11,9 +12,19 @@ function draw() {
   background('#fefae0'); // 設定背景顏色
 
   // 計算影像的顯示位置，讓它位於畫布正中間
-  let x = (width - capture.width) / 2;
-  let y = (height - capture.height) / 2;
+  let xOffset = (width - capture.width) / 2;
+  let yOffset = (height - capture.height) / 2;
 
-  // 繪製攝影機影像
-  image(capture, x, y, capture.width, capture.height);
+  // 繪製馬賽克效果
+  for (let y = 0; y < capture.height; y += mosaicSize) {
+    for (let x = 0; x < capture.width; x += mosaicSize) {
+      // 取得區塊的顏色
+      let c = capture.get(x, y);
+
+      // 設定填充顏色並繪製方塊
+      fill(c);
+      noStroke();
+      rect(x + xOffset, y + yOffset, mosaicSize, mosaicSize);
+    }
+  }
 }
